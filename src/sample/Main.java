f4f97fc28c45d10271f0d10e44e7cb3d0903d1fa
Frej Wichmann <frejwichmann@gmail.com>
2019-11-06 14:36:42 +0100

    m

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,8 +1,14 @@
 package sample;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Skriv in ditt slag");
+        int i = scanner.nextInt();
     }
 }
